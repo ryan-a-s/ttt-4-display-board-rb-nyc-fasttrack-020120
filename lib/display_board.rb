b0e1d263549a68ab84ca23b62a3f8d4d7d9d4f2e
Ryan Seit <ryan@ryanseit.com>
2020-02-01 18:49:29 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -3,7 +3,7 @@
 
 board = [" ", " ", " ", " ", " ", " ", " ", " ", " ",]
 
-def display_board(the_board)
+def display_board(board)
   puts " #{the_board[0]} | #{the_board[1]} | #{the_board[2]} "
   puts "-----------"
   puts " #{the_board[3]} | #{the_board[4]} | #{the_board[5]} "
